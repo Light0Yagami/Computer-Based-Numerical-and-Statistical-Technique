ee9b41ebc0444f308c044f5bf57bef8148b5888a
Ayush negi <79302271+Light0Yagami@users.noreply.github.com>
2022-09-12 15:24:10 +0530

    Create 02. Regula Falsi.c

---
+++ 02. Regula Falsi.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+
+float fun(float x){
+	float ans = x*x*x - 5*x +1;
+	return ans;
+}
+
+int main(){
+	float a,b;
+	printf("Enter the value of a and b: ");
+	scanf("%f%f",&a,&b);
+
+	if(fun(a) * fun(b)< 0){
+		printf("Range is correct\n");
+		float allowed_err;
+		printf("Enter the allowed error: ");
+		scanf("%f",&allowed_err);
+
+		float x = a - (((b-a)/(fun(b) - fun(a))) * fun(a));
+		while(fabs(fun(x)) > allowed_err){
+			if(fun(a)*fun(x) < 0) b = x;
+			else if (fun(x) * fun(b) < 0) a = x;
+			x = a - ((b-a)/(fun(b) - fun(a))) * fun(a);
+			printf("x = %f f(x) = %f\n",fabs(x),fun(x));
+		}
+		printf("x = %f f(x) = %f\n",fabs(x),fun(x));
+	}
+	else{
+		printf("Range is incorrect");
+	}
+	
+	return 0;
+}
