472ac820647e39d97297d8d8931ce47b3f0cd674
Ayush negi <79302271+Light0Yagami@users.noreply.github.com>
2022-10-09 15:32:27 +0530

    Create 06. GuassJordan.c

---
+++ 06. GuassJordan.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void touppert(float a[20][20],int n){
+ for(int i = 0; i < n; i++){
+     for(int j = 0; j < n; j++){
+         if(j != i){
+             double c = a[j][i]/a[i][i];
+             for(int k = 0; k < n+1; k++){
+                 a[j][k] = a[j][k] - ( c * a[i][k]);
+             }
+         }
+     }
+ }
+}
+
+void print(float mat[20][20], int n){
+  printf("UpperTriangular Matrix:\n");
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j <= n; j++){
+      printf("%f  ",mat[i][j]);
+    }
+    printf("\n");
+  }
+  
+}
+
+int main(){
+  
+  float mat[20][20];
+  
+  int n,i,j;
+  printf("\nEnter the size of matrix\n");
+  scanf("%d",&n);
+  
+  matrix:
+    printf("\nEnter the matrix\n");
+    for(i= 0; i < n; i++){
+      for(j = 0; j <= n; j++){
+        scanf("%f",&mat[i][j]);
+      }
+    }
+  
+  if(mat[0][0] == 0){
+    printf("1st element cannot be zero\n");
+    printf("Again ");
+    goto matrix;
+  }
+  
+  touppert(mat,n);
+  printf("\n");
+  print(mat,n);
+  printf("\n");
+  
+  
+  
+  for(i = 0; i < n;i++){
+      
+      printf("%d : %f\n",i,mat[i][n]/mat[i][i]);
+  }
+  
+  
+ 
+
+  return 0;
+}
